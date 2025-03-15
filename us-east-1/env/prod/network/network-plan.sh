e89c102202405938bf1bb4fab675811118f5442f
AyrtonFidel7 <ayrton.avalos@workersbenefitfund.link>
2025-03-14 23:51:55 -0500

    Network ci/cd v2.1

---
+++ us-east-1/env/prod/network/network-plan.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+# network-apply: script to apply aws resources using terraform 
+
+echo "Applying VPC..."
+sleep 5
+cd 01-vpc/
+
+terraform init -backend-config=vpc-backend.hcl
+terraform plan -var-file=vpc.tfvars
+cd ..
+sleep 2
+
+echo "Applying Subnets..."
+sleep 5
+cd 02-subnets/
+terraform init -backend-config=subnets-backend.hcl
+terraform plan -var-file=subnets.tfvars
+cd ..
+sleep 2
+
+echo "Applying Internet Gateway..."
+sleep 5
+cd 03-internet-gateway
+terraform init -backend-config=igw-backend.hcl
+terraform plan -var-file=igw.tfvars
+cd ..
+sleep 2
+
+echo "Applying Nat Gateways..."
+sleep 5
+cd 04-nat-gateway/
+terraform init -backend-config=nat-backend.hcl
+terraform plan -var-file=nat.tfvars
+cd ..
+sleep 2
+
+echo "Applying Route tables..."
+sleep 5
+cd 05-route-tables/
+terraform init -backend-config=rtb-backend.hcl
+terraform plan -var-file=rtb.tfvars
+cd ..
+sleep 2
+
+
+echo "Applying Endpoints..."
+sleep 5
+cd 06-endpoints/
+terraform init -backend-config=endpoints-backend.hcl
+terraform plan
+cd ..
+sleep 2
+
+echo "ALL NETWORK INFRASTRUCTURE PLANNED SUCCESSFULLY"
