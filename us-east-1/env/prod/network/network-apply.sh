792437378ea1e93f9d22b352385cafd90ba78170
AyrtonFidel7 <ayrton.avalos@workersbenefitfund.link>
2025-03-15 13:26:41 -0500

    API gateway workflow v3.11

--- us-east-1/env/prod/network/network-apply.sh
+++ us-east-1/env/prod/network/network-apply.sh
@@ -4,7 +4,7 @@
 echo "Applying VPC..."
 sleep 5
 cd 01-vpc/
-
+export AWS_PROFILE=ayrton
 terraform init -backend-config=vpc-backend.hcl
 terraform apply -var-file=vpc.tfvars --auto-approve
 cd ..
@@ -13,6 +13,7 @@ sleep 2
 echo "Applying Subnets..."
 sleep 5
 cd 02-subnets/
+export AWS_PROFILE=ayrton
 terraform init -backend-config=subnets-backend.hcl
 terraform apply -var-file=subnets.tfvars --auto-approve
 cd ..
@@ -21,6 +22,7 @@ sleep 2
 echo "Applying Internet Gateway..."
 sleep 5
 cd 03-internet-gateway
+export AWS_PROFILE=ayrton
 terraform init -backend-config=igw-backend.hcl
 terraform apply -var-file=igw.tfvars --auto-approve
 cd ..
@@ -29,6 +31,7 @@ sleep 2
 echo "Applying Nat Gateways..."
 sleep 5
 cd 04-nat-gateway/
+export AWS_PROFILE=ayrton
 terraform init -backend-config=nat-backend.hcl
 terraform apply -var-file=nat.tfvars --auto-approve
 cd ..
@@ -37,6 +40,7 @@ sleep 2
 echo "Applying Route tables..."
 sleep 5
 cd 05-route-tables/
+export AWS_PROFILE=ayrton
 terraform init -backend-config=rtb-backend.hcl
 terraform apply -var-file=rtb.tfvars --auto-approve
 cd ..
@@ -46,6 +50,7 @@ sleep 2
 echo "Applying Endpoints..."
 sleep 5
 cd 06-endpoints/
+export AWS_PROFILE=ayrton
 terraform init -backend-config=endpoints-backend.hcl
 terraform apply --auto-approve
 cd ..
--- us-east-1/env/prod/network/network-plan.sh
+++ us-east-1/env/prod/network/network-plan.sh
@@ -4,50 +4,55 @@
 echo "Planning' VPC..."
 sleep 5
 cd 01-vpc/
-
+export AWS_PROFILE=ayrton
 terraform init -backend-config=vpc-backend.hcl
-terraform plan -var-file=vpc.tfvars --profile ayrton
+terraform plan -var-file=vpc.tfvars
 cd ..
 sleep 2
 
 echo "Planning Subnets..."
 sleep 5
+export AWS_PROFILE=ayrton
 cd 02-subnets/
 terraform init -backend-config=subnets-backend.hcl
-terraform plan -var-file=subnets.tfvars --profile ayrton
+terraform plan -var-file=subnets.tfvars
 cd ..
 sleep 2
 
 echo "Planning Internet Gateway..."
 sleep 5
 cd 03-internet-gateway
+export AWS_PROFILE=ayrton
 terraform init -backend-config=igw-backend.hcl
-terraform plan -var-file=igw.tfvars --profile ayrton
+terraform plan -var-file=igw.tfvars
 cd ..
 sleep 2
 
 echo "Planning Nat Gateways..."
 sleep 5
 cd 04-nat-gateway/
+export AWS_PROFILE=ayrton
 terraform init -backend-config=nat-backend.hcl
-terraform plan -var-file=nat.tfvars --profile ayrton
+terraform plan -var-file=nat.tfvars
 cd ..
 sleep 2
 
-echo "Planning Route tables..."
+echo "Planning' Route tables..."
 sleep 5
 cd 05-route-tables/
+export AWS_PROFILE=ayrton
 terraform init -backend-config=rtb-backend.hcl
-terraform plan -var-file=rtb.tfvars --profile ayrton
+terraform plan -var-file=rtb.tfvars
 cd ..
 sleep 2
 
 
-echo "Planning Endpoints..."
+echo "Planning' Endpoints..."
 sleep 5
 cd 06-endpoints/
+export AWS_PROFILE=ayrton
 terraform init -backend-config=endpoints-backend.hcl
-terraform plan --profile ayrton
+terraform plan
 cd ..
 sleep 2
 
