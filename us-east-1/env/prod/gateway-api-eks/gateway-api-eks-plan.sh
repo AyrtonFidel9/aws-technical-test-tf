072b400619c51b34578b8502bafb34421cda907e
AyrtonFidel7 <ayrton.avalos@workersbenefitfund.link>
2025-03-15 10:46:40 -0500

    API gateway workflow created

---
+++ us-east-1/env/prod/gateway-api-eks/gateway-api-eks-plan.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+# eks-apply: script to apply aws eks (k8s) resources using terraform 
+
+echo "Planning IAM PERMISSIONS..."
+sleep 5
+cd 01-iam-permissions/
+terraform init -backend-config=eks-iam-backend.hcl
+terraform plan --var-file=eks-iam.tfvars
+terraform state pull > terraform.tfstate
+cd ..
+sleep 2
+
+echo "Planning Cluster Settings..."
+sleep 5
+cd 02-cluster/
+terraform init -backend-config=cluster-backend.hcl
+terraform plan --var-file=cluster.tfvars
+terraform state pull > terraform.tfstate
+cd ..
+sleep 2
+
+echo "Planning Node Groups..."
+sleep 5
+cd 03-node-group
+terraform init -backend-config=ng-backend.hcl
+terraform plan --var-file=node-group.tfvars
+terraform state pull > terraform.tfstate
+cd ..
+sleep 2
+
+echo "Planning ADD-ONS..."
+sleep 5
+cd 04-addons/
+terraform init -backend-config=addons-backend.hcl
+terraform plan --var-file=addons.tfvars
+terraform state pull > terraform.tfstate
+cd ..
+sleep 2
+
+
+echo "ALL KUBERNETES INFRASTRUCTURE PLANNED SUCCESSFULLY"
