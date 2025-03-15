e5c964081bad640dcd517f78f53b279cb8541591
AyrtonFidel7 <ayrton.avalos@workersbenefitfund.link>
2025-03-15 00:18:24 -0500

    Network ci/cd v2.7

--- us-east-1/env/prod/network/network-destroy.sh
+++ us-east-1/env/prod/network/network-destroy.sh
@@ -4,6 +4,7 @@
 echo "Destroying Endpoints..."
 sleep 5
 cd 06-endpoints/
+terraform init -backend-config=endpoints-backend.hcl
 terraform destroy --auto-approve
 terraform state pull > terraform.tfstate
 cd ..
@@ -12,6 +13,7 @@ sleep 2
 echo "Destroying Route tables..."
 sleep 5
 cd 05-route-tables/
+terraform init -backend-config=rtb-backend.hcl
 terraform destroy -var-file=rtb.tfvars --auto-approve
 rm -r terraform.tfstate
 cd ..
@@ -20,6 +22,7 @@ sleep 2
 echo "Destroying Nat Gateways..."
 sleep 5
 cd 04-nat-gateway/
+terraform init -backend-config=nat-backend.hcl
 terraform destroy -var-file=nat.tfvars --auto-approve
 rm -r terraform.tfstate
 cd ..
@@ -28,6 +31,7 @@ sleep 2
 echo "Destroying Internet Gateway..."
 sleep 5
 cd 03-internet-gateway
+terraform init -backend-config=igw-backend.hcl
 terraform destroy -var-file=igw.tfvars --auto-approve
 rm -r terraform.tfstate
 cd ..
@@ -36,6 +40,7 @@ sleep 2
 echo "Destroying Subnets..."
 sleep 5
 cd 02-subnets/
+terraform init -backend-config=subnets-backend.hcl
 terraform destroy -var-file=subnets.tfvars --auto-approve
 rm -r terraform.tfstate
 cd ..
@@ -44,6 +49,7 @@ sleep 2
 echo "Destroying VPC..."
 sleep 5
 cd 01-vpc
+terraform init -backend-config=vpc-backend.hcl
 terraform destroy -var-file=vpc.tfvars --auto-approve
 rm -r terraform.tfstate
 cd ..
